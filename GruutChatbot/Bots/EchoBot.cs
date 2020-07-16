c275cd47d95a044fa6abefc7e00148a8035cde8e
shahedc <shahedc@gmail.com>
2020-07-16 13:07:57 -0400

    initial projected generated from Bot Framework v4 template, updated with Gruut language

---
+++ GruutChatbot/Bots/EchoBot.cs
@@ -0,0 +1,43 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.9.2
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+
+namespace GruutChatbot.Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            Random rand = new Random();
+            var replyText = string.Empty;
+
+            if (rand.Next(0, 2) == 0)
+                replyText = "I am Gruut?";
+            else
+                replyText = "I am Gruut.";
+
+            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var welcomeText = "I am Gruut!";
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                }
+            }
+        }
+    }
+}
